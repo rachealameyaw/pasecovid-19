aa9cc8802604a474473ef3707e63176dc5b0743e
Racheal <maamekusiwaa30@gmail.com>
2020-08-18 16:58:31 +0000

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import ReactDOM from 'react-dom';
+import './Mee.css'
+import App from './App';
+
+
+
+
+ReactDOM.render(
+
+    <App />,
+  document.getElementById('root')
+);
+
