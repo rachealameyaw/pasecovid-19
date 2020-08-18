dc67691c59134192451a5aaf62eeb44de630eacf
Racheal <maamekusiwaa30@gmail.com>
2020-08-18 16:56:45 +0000

    Create Cardbar.js

---
+++ src/components/Cardbar.js
@@ -0,0 +1,41 @@
+// import React, { Component } from 'react'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import Mee1 from '../img1/img1.jpg'
+// import Mee2 from '../img1/img4.jpg'
+// import Mee3 from '../img1/img.jpg'
+
+
+
+
+
+// class Cardbar extends Component{
+//     render(){
+//         return(
+//             <div>
+//                  <div className="col-md-4">
+//                                                     <div className="card">
+//                                                         <div className="card-body">
+//                                                             <h4 className="card=title"><a href="#" className="btn btn-danger"> Health Tips</a></h4>
+//                                                             <img src={Mee1} alt="mee" className="img-fluid img-thumbnail"/>
+//                                                         </div> <br/>
+
+
+                                                        
+//                                             <div className="card">
+//                                                         <div className="card-body">
+//                                                             <h4 className="card=title"><a href="#" className="btn btn-danger"> News & Updates</a></h4>
+//                                                             <img src={Mee2} alt="mee" className="img-fluid img-thumbnail"/ >
+//                                                             <p className="card-text">Covid-19 at the low <br/><img src={Mee3}alt=""/> </p>
+//                                                         </div>
+//                                                     </div>
+//                                                     </div> 
+                                                   
+//                                             </div>
+                                          
+            
+//             </div>
+//         )
+//     }
+// }
+
+// export default Cardbar
